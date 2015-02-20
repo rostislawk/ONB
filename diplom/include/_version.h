1144d21f63334a6ab5199e711e3fa26b7b986d50
Rostislav <rostislawk@mail.ru>
2015-02-20 14:47:56 +0300

    initial commit

---
+++ diplom/include/_version.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------
+  VERSION CONTROL BUILD SYSTEM                               
+  This header file was created by VERBUILD v1.0.1            
+  -----------------------------------------------------------
+  help : verbuild -?                                         
+  info : http://www.yeamaec.com                              
+         yeamaec@hanafos.com ,krkim@yeamaec.com              
+-----------------------------------------------------------*/
+
+#ifndef __VERSION
+#define __VERSION
+
+#define VERSION_FULL           2.0.3049.464
+
+#define VERSION_BASEYEAR       2012
+#define VERSION_DATE           "2015-02-19"
+#define VERSION_TIME           "23:13:35"
+
+#define VERSION_MAJOR          2
+#define VERSION_MINOR          0
+#define VERSION_BUILDNO        3049
+#define VERSION_EXTEND         464
+
+#define VERSION_FILE           2,0,3049,464
+#define VERSION_PRODUCT        2,0,3049,464
+#define VERSION_FILESTR        "2,0,3049,464\0"
+#define VERSION_PRODUCTSTR     "2,0,3049,464\0"
+
+#endif
---
+++ diplom/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "core/mem.h"
+#include "core/util.h"
+#include "crypto/belt.h"
+
+#ifdef OS_WINDOWS
+	#include <locale.h>
+	#include <windows.h>
+#endif
+
+int main ()
+{
+	return 0;
+}
